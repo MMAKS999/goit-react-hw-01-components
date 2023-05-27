283c90f298e06a5b96d29c684221ce0e7bec56ba
MAX999 <max999b@gmail.com>
2023-05-27 18:13:05 +0300

    profile
    без перевірки на тип даних

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,31 @@
+import { Profile } from "./profile";
+import  user  from "../data/user.json";
+
+
+
+
+
 export const App = () => {
   return (
     <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
+       // style={{
+      //   height: '100vh',
+      //   display: 'flex',
+      //   justifyContent: 'center',
+      //   alignItems: 'center',
+      //   fontSize: 40,
+      //   color: '#010101'
+      // }}
     >
-      React homework template
+      <Profile
+  username={user.username}
+  tag={user.tag}
+  location={user.location}
+  avatar={user.avatar}
+  stats={user.stats}
+/>
+      {/* React homework template */}
     </div>
   );
 };
+
